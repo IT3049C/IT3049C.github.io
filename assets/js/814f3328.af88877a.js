171928f26e3450e123edeb28cabf17a6c96de008
Yahya Gilany <yahya.gilany@live.com>
2022-01-14 21:54:54 -0500

    Deploy website - based on 12acde833741e47253b93014f738566be69779cf

--- assets/js/814f3328.af88877a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Visual Studio Code Settings and Extensions Sync","permalink":"/blog/vscode-extensions"}]}')}}]);
